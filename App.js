21f9a5be630e9d26fda95d01251bb4410d398af7
prasannakumar50 <babblu293@gmail.com>
2024-02-10 17:34:56 +0530

    Implented functional component

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM  from "react-dom";
 
 
-const heading = React.createElement("h1", {}, "Hello React");
-console.log(heading);
+const FunctionComponent = () =>(
+    <div>
+       <h1>Its Functional Component</h1>
+    </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<FunctionComponent />);
